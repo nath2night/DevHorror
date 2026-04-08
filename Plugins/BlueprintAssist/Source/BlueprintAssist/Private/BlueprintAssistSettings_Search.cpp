2e24032f2d84f98f7c1a6364edcd302e0e3f0c6c
nath2night <nil.percebois@student.studio-m.fr>
2026-04-08 21:55:49 +0200

    vhs et autre

--- Plugins/BlueprintAssist/Source/BlueprintAssist/Private/BlueprintAssistSettings_Search.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Copyright fpwong. All Rights Reserved.
-
-
-#include "BlueprintAssistSettings_Search.h"
-
-UBlueprintAssistSettings_Search& UBlueprintAssistSettings_Search::Get()
-{
-	return *GetMutableDefault<UBlueprintAssistSettings_Search>();
-}
